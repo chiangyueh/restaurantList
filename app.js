e08ecd6d0fb87d81df9b9be8c94a936aa723be3c
yueh <y456123u@gmail.com>
2022-12-06 02:53:44 +0800

    新增restaurantList項目

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const exphbs = require('express-handlebars');
+const restaurantList = require('./restaurant.json')
+app.engine('handlebars',exphbs({ defaultLayout : 'main'}));
+app.set('view engine','handlebars');
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+    const restaurantlist = restaurantList.results;
+    res.render('index',{restaurantlist});
+})
+
+app.get('/search',(req,res)=>{
+    const keyword = req.query.keyword;
+    const restaurantlist = restaurantList.results.filter(res=>{
+        return res.name.toLowerCase().includes(keyword.toLowerCase())
+    });
+    console.log(keyword)
+    res.render('index',{restaurantlist});
+})
+
+app.get('/restaurants/:num',(req,res)=>{
+    const restaurantlist = restaurantList.results.find(
+        res => res.id.toString() === req.params.num
+        );
+        console.log(restaurantlist)
+    res.render('show',{restaurantlist});
+})
+app.listen(port, () => {
+    console.log(port)
+})
