37f632197fa47f241331a65649ea7277382a8068
yueh <y456123u@gmail.com>
2022-12-06 17:48:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,25 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const exphbs = require('express-handlebars');
-const restaurantList = require('./restaurant.json')
+const restaurantData = require('./restaurant.json').results;
+
 app.engine('handlebars',exphbs({ defaultLayout : 'main'}));
 app.set('view engine','handlebars');
 app.use(express.static('public'));
+
 app.get('/',(req,res)=>{
-    const restaurantlist = restaurantList.results;
-    res.render('index',{restaurantlist});
+    res.render('index',{restaurantlist : restaurantData});
 })
 
 app.get('/search',(req,res)=>{
-    const { keyword } = req.query;
-    const restaurantlist = restaurantList.results.filter(res=>{
-        return res.name.toLowerCase().includes(keyword.toLowerCase())
-    });
+    const keyword = req.query.keyword.trim().toLowerCase();
+    if(!keyword){
+        return res.redirect("/");
+    }
+    const restaurantlist = restaurantData.filter(res=>res.name.toLowerCase().includes(keyword));
     res.render('index',{restaurantlist});
 })
 
 app.get('/restaurants/:num',(req,res)=>{
-    const restaurantlist = restaurantList.results.find(
+    const restaurantlist = restaurantData.find(
         res => res.id.toString() === req.params.num
         );
     res.render('show',{restaurantlist});
